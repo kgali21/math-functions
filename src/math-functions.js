7e028d669be81659250d71c592c7b9f68f429c40
Kamran Ali <kgali21@gmail.com>
2019-07-19 14:46:00 -0700

    solved sum function

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,10 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    let added = a + b;
+    let sum = 'The sum of ' + a + ' and ' + b + ' is ' + added + '.';
+    
+    return [added, sum];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
